1104f9780ded864237988ff3ceb124b792937805
Maksim Evstafev <emaxiss@gmail.com>
2020-09-21 23:09:03 -0400

    new class default constructor

--- HelloStudents/src/foundation/TestStudent.java
+++ HelloStudents/src/foundation/TestStudent.java
@@ -4,6 +4,13 @@ public class TestStudent
 {
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		Student st1 = new Student();
+		st1.eat();
+		st1.setName("Vasya");
+		System.out.println(st1.getName());
+		
+		Student st2 = new Student();
+		st2.eat();
+		
 	}
 }
