0263eb98f26d50570b102468c53bc01775554ac7
Maksim Evstafev <emaxiss@gmail.com>
2020-09-30 23:09:02 -0400

    homework1,2

---
+++ HelloStudents/src/homeworks/H1b.java
@@ -0,0 +1,22 @@
+package homeworks;
+
+public class H1b
+{
+	public void vowels(char ch)
+	{
+		switch(ch)
+		{
+			case 'a', 'e', 'i', 'o', 'u' -> System.out.println("Vowel");
+			default -> System.out.println("Consonant");
+		}
+	}
+	public static void main(String[] args)
+	{
+		H1b h = new H1b();
+		char ch = 'a';
+		h.vowels(ch);
+		
+		ch = 'b';
+		h.vowels(ch);
+	}
+}
