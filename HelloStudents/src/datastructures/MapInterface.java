55433ecb681220717b8a25cfce305f5333f1ba4a
Maksim Evstafev <emaxiss@gmail.com>
2020-10-29 00:33:30 -0400

    updated after projects removal

---
+++ HelloStudents/src/datastructures/MapInterface.java
@@ -0,0 +1,33 @@
+package datastructures;
+import java.util.HashMap;
+import java.util.Map;
+public class MapInterface
+{
+	public static void main(String[] args)
+	{
+		Map<String, Integer> animals = new HashMap<String, Integer>();
+		animals.put("dog", 1);
+		animals.put("horse", 1);
+		animals.put("rabbit", 68);
+		
+		int dogNumber = animals.get("dog");
+		System.out.println(dogNumber);
+		int size = animals.size();
+		
+		System.out.println("Number of the map entries: " + size);
+		
+		if (animals.containsKey("rabbit"))
+			System.out.println("Zoo has rabbits");
+		
+		System.out.println(animals.containsValue(1));
+		
+		animals.forEach((k, v) -> System.out.println(k + " = " + v));
+		
+		Map zoo = Map.of("dog", 1,
+						"horse", 1,
+						"rabbit", 68);
+		
+		int horseNumber = (int)zoo.get("horse");
+		System.out.println("horse number: " + horseNumber);
+	}
+}
--- HelloStudents/src/files/ReadConfigFile.java
+++ HelloStudents/src/files/ReadConfigFile.java
@@ -17,3 +17,4 @@ public class ReadConfigFile
 		System.out.println(browser + " " + os);
 	}
 }
+
