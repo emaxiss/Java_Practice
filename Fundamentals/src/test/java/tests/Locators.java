bf66035337997fc62dc3488acf63564acfb657e7
Maksim Evstafev <emaxiss@gmail.com>
2020-11-26 00:32:46 -0500

    locators

--- Fundamentals/src/test/java/tests/Locators.java
+++ Fundamentals/src/test/java/tests/Locators.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import common.BaseTest;
 
@@ -10,6 +11,19 @@ class Locators extends BaseTest
 	@Test
 	void test()
 	{
-		driver.navigate().to("https://www.kmart.com/");
+		driver.navigate().to("https://www.kmart.com/");  
+		
+// BY class Name		
+		driver.findElement(By.className("ribbon-kmart-logo")).click();  
+		
+// By CSS Selector		
+		driver.findElement(By.cssSelector("#yourAccount")).click();     
+		driver.navigate().refresh();
+		
+// By Id
+		driver.findElement(By.id("keyword")).sendKeys("Milk");
+		
+// By link text
+		driver.findElement(By.linkText("Kmart home")).click();
 	}
 }
